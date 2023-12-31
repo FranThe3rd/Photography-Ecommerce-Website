aec23aa160bbec1523b485edd72140bb9d4fbd4a
FranThe3rd <francisco21xd234@gmail.com>
2023-12-30 23:36:16 -0500

    not worth to be talked about

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import homePageImage from '../assets/homeImg/home.jpg'
 import './home.css'
+
 const Home = () => {
   return (
     <div className='home'>
-      <img  src={homePageImage} alt="" />
-    <div className='home-container'>
+      <img src={homePageImage} alt="" />
+      <div className='home-container'></div>
+      <div className='home-info'>
+        <h1>Juan</h1>
+        <Link to="/shop" className='exploreBttn' role='button'>
+          Explore Page
+        </Link>
+      </div>
     </div>
-    <div className='home-info'>
-    <h1>Juan 
-    </h1>
-    <button className='exploreBttn' role='button'>Explore Page</button>
-    </div>
-    </div>
-
   )
 }
 
