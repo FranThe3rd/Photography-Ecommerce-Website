4d9f3488d7bfd27402712f59ead34e23c7b7d246
figueroaf <figueroaf@moravian.edu>
2024-01-01 03:38:56 -0500

    made the about me page look better

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -6,17 +6,21 @@ import { InstagramLogo } from 'phosphor-react'
 const About = () => {
   return (
     <div className='about'>
-      <h1>About</h1>
-      <div className='about-info'>
-        <img src={myImage} alt="" />
-        <h3>
-          Hello! I'm Juan Landaverde, a photographer with a deep appreciation for the natural world. Originally from El Salvador, I now call Pennsylvania home. My journey behind the lens is fueled by a profound connection to the diverse landscapes and wildlife that have shaped my life.
-          Specializing in nature photography, I aim to capture the essence of my surroundings. From the vibrant scenes of El Salvador to the tranquil beauty of Pennsylvania, my photos seek to convey the awe-inspiring moments found in the ordinary.
-          "Live and let Live" is more than a mantra—it's a philosophy that guides my work. Each image reflects not only the beauty of nature but also the delicate balance we share with it.
-          This website is a platform to share my visual stories with you. Consider bringing a touch of nature into your space through the purchase of prints. Your support allows me to continue exploring and documenting the incredible world we inhabit.
-        </h3>
+      <h1 className='title'>About</h1>
+      <div className='about-wrapper'>
+        <div className='about-image'>
+          <img src={myImage} alt="" />
+        </div>
+        <div className='about-text'>
+          <h2 className='h2-title'>Hello, I am Juan Landaverde</h2>
+          <h3 className='h3-explain'>
+          I'm a nature photographer originally from El Salvador, now based in Pennsylvania. My lens captures the intricate beauty of the natural world, focusing on birds in their habitats and the captivating allure of waterfalls. From the vibrant plumage of tropical birds to the serene flow of hidden waterfalls, my goal is to showcase the diverse wonders of our planet. Through my work, I aim to inspire appreciation for nature and encourage the protection of our precious ecosystems.</h3>
+          <h3>Instagram: <a className='insta' href='https://www.instagram.com/ijuanlandaverde/' target='_blank'>@ijuanlandaverde</a></h3>
+          <h3>Email: juan_landaverde2001@yahoo.com</h3>
+        </div>
       </div>
     </div>
+
   )
 }
 
