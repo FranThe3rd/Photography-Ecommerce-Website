d6f46de903fe71be816dcba13a8866497d7703c3
FranThe3rd <francisco21xd234@gmail.com>
2024-01-05 17:50:56 -0500

    change a couple of images and buttons styling

--- src/pages/shop/products.js
+++ src/pages/shop/products.js
@@ -1,6 +1,11 @@
 import AntleredElegance from '../../assets/productImg/maleDeer1.png';
 import HomelessRailroads from '../../assets/productImg/railRoad1.png';
-import Waterfall from '../../assets/productImg/waterFall1.png';
+import WaterfallGreatness from '../../assets/productImg/waterFall1.png';
+import AmericaBird from '../../assets/productImg/BaldEagle1.png';
+import BearEssentials from '../../assets/productImg/BlackBear1.png';
+import Osprey from '../../assets/productImg/Osprey1.png';
+import Forest from '../../assets/productImg/Forest1.png';
+import GoldFinch from '../../assets/productImg/GoldFinch1.png';
 
 export const PRODUCT_LIST = [
     {
@@ -8,58 +13,47 @@ export const PRODUCT_LIST = [
         productName: "Antlered Elegance",
         price: 125.24,
         productImage: AntleredElegance
-    
     },
-
     {
         id: 2,
         productName: "Homeless Railroads",
-        price: 125.24,
+        price: 99.99,
         productImage: HomelessRailroads
     },
-
     {
         id: 3,
-        productName: "Antlered Elegance",
-        price: 125.24,
-        productImage: Waterfall
-    
+        productName: "Waterfall Greatness",
+        price: 79.99,
+        productImage: WaterfallGreatness
     },
-
     {
         id: 4,
-        productName: "Homeless Railroads",
-        price: 125.24,
-        productImage: HomelessRailroads
+        productName: "America's Bird",
+        price: 149.99,
+        productImage: AmericaBird
     },
-
     {
         id: 5,
-        productName: "Antlered Elegance",
-        price: 125.24,
-        productImage: AntleredElegance
-    
+        productName: "Bear Essentials",
+        price: 109.99,
+        productImage: BearEssentials
     },
-
     {
         id: 6,
-        productName: "Homeless Railroads",
-        price: 125.24,
-        productImage: HomelessRailroads
-    }, 
-
+        productName: "Majestic Osprey",
+        price: 129.99,
+        productImage: Osprey
+    },
     {
         id: 7,
-        productName: "Antlered Elegance",
-        price: 125.24,
-        productImage: AntleredElegance
-    
+        productName: "Enchanted Forest",
+        price: 89.99,
+        productImage: Forest
     },
-
     {
         id: 8,
-        productName: "Homeless Railroads",
-        price: 125.24,
-        productImage: HomelessRailroads
-    }
-]
+        productName: "Golden Sunrise Finch",
+        price: 69.99,
+        productImage: GoldFinch
+    },
+];
