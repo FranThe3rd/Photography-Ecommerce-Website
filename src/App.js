15b9971999fee85aae4e7495cb6d5a93877577c6
figueroaf <figueroaf@moravian.edu>
2024-01-24 19:50:46 -0500

    created a responsive navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Shop from './pages/shop/shop';
 import Home from './pages/home';  
 import Cart from './pages/cart/cart';  
-import About from './pages/about/about';
 import Contact from './pages/contact/contact';
 import './App.css';
-import NavbarMain from './components/navbarOne/navbarMain';
-import NavbarTwo from './components/navbarTwo/navbarTwo';
+import Navbar from './components/navbar/navbar';
 import ShopContextProvider from './context/shop-context';
 
 
@@ -19,10 +17,10 @@ function App() {
     <div className="App">
       <ShopContextProvider>
 <Router location={disableTransitions ? {transition: 'none'} : {}}>
-        <div className='appNav'>
-        <NavbarMain/>
-        <NavbarTwo/>
-        </div>
+  <div className='appNav'>
+  <Navbar/>
+
+  </div>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/shop" element={<Shop/>}/>
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { List } from 'phosphor-react'
+import { InstagramLogo } from 'phosphor-react'
+import { ShoppingCart } from 'phosphor-react'
+
+
+import './navbar.css'
+
+
+const Navbar = () => {
+    const [showNavbar, setShowNavbar] = useState(false)
+
+
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar)
+    }
+
+
+    return (
+        <nav className="navbar">
+            <div className="container-nav">
+                <div className="logo">
+                    <h1>Juan Landaverde</h1>
+                </div>
+                <div className="menu-icon" onClick={handleShowNavbar}>
+                    <List size={40} color='black' />
+                </div>
+                <div className={`nav-elements  ${showNavbar && 'active'}`}>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/shop">Shop</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact">Contact</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/cart">
+                                <ShoppingCart size={30} color='black' />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="https://www.instagram.com/ijuanlandaverde/" target="_blank" rel="noopener noreferrer">
+                                <InstagramLogo size={30} color='black' />
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+
+export default Navbar
